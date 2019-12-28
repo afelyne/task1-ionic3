6a889247346f7d3b8d11f548e288b7d47fa0b1dd
blackbird13 <afelyne@gmail.com>
2019-12-27 22:57:27 -0600

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { RegistroPage} from '../registro/registro';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  irRegistro(){
+    this.navCtrl.push( RegistroPage );
+  }
+
+}
